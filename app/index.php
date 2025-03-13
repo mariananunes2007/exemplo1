4bdc7990ffc8e29c4f7850490bf07fc76f37b36a
mariananunes2007 <mn534997@gmail.com>
2025-03-13 01:44:02 +0000

    Aula 12/03/2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,35 @@
 <?php
-// require './vendor/autoload.php';
+//$nome = $_GET['nome'];
+$nome = '';
+$periodo = '';
+if($_SERVER['RESQUEST_METHOD'] == 'POST') {
+    $nome = $POST['nome'];
+    $periodo = $_POST['periodo'];
+}
+?>
 
-// use Application\DBConnection\MySQLConnection;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Primeira Aplicação</title>
+</head>
+    <body>
+        <h1>
+            <?php if($periodo == 'm'): ?>
+                Bom dia <?= $nome ?>
 
-// $db = new MySQLConnection();
-
-echo phpinfo();
+        </h1>
+        <form action="/idex.php" method="post"> 
+            <label for="periodo">Selecione o Período</label>
+            <select name="periodo">
+                <option value="m">Manhã</option>
+                <option value="t">Tarde</option>
+                <option value="n">Noite</option>
+            </select>
+            <label for="nome">Digite seu nome</label>
+            <input type="text" name="nome" />
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
